226041ef51e2c9dfc97961cf168f1906882987e4
tall-horse <dima.gamedev@pm.me>
2025-03-04 12:00:04 +0100

    value binding and immutability

---
+++ values.fsx
@@ -0,0 +1,19 @@
+let cityName = "New Yok"
+
+let fSharpIsAwesome = true
+
+let luckyNumber = 13
+
+let coordinates = (1.0, 3.0)
+
+let toDoList = ["Learn F#"; "Build app"; "Profit!"]
+
+let divideByThree input = input / 3
+
+let (luckyNumberString:string) = "13"
+
+let correctCityName = "New York";
+
+let mutable mutableCityName = "New Yok"
+
+mutableCityName <- "New York"
